6c0b939c37083d2228f557dbe2af7a02ae02e847
cindy-pan-ics4u1-02-25 <cindy.pan@student.tdsb.on.ca>
2025-02-21 04:24:26 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,46 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static String pyramid(int num){
+		String astrix = "";
+		if (num==1){
+			astrix = astrix+"*";
+		}
+		else {
+		for (int row = 1; row<=num; row++){
+			for (int star = (0); star < row; star++){
+				if (row == 1){
+					astrix = astrix + " ";
+					astrix = astrix +" *";
+				}
+				else if (row==num&&star==0){
+					astrix = astrix + "*";
+				}
+				else {
+				astrix= (astrix+" *");
+				}
+			}
+			if (row!=num){
+			astrix = (astrix+"\n");
+			}
+		}
+	}
+		return astrix;
 	}
 
+
+	public static String square(int num){
+		String astrix = "";
+		for (int row = num; row<=num&&row>=0; row--){
+			for (int star = 0; star < row; star++){
+				if (row == num){
+					astrix = astrix +"*";
+				}
+			}
+		}
+		return astrix;
+	}
+
+	public static boolean hasLowercase(String word){
+		return true;
+	}
 }
