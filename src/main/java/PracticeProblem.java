9d3c1c8149028fe6b3401a445dc1cb66df76d548
cindy-pan-ics4u1-02-25 <cindy.pan@student.tdsb.on.ca>
2025-02-23 23:29:10 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -30,17 +30,57 @@ public class PracticeProblem {
 
 	public static String square(int num){
 		String astrix = "";
-		for (int row = num; row<=num&&row>=0; row--){
-			for (int star = 0; star < row; star++){
-				if (row == num){
-					astrix = astrix +"*";
+		int numStars=0;
+		if (num ==1){
+			astrix = astrix +"*";
+		}
+		else {
+				for (int rowsOf = 1; rowsOf<=num; rowsOf++){
+					if (rowsOf==1){
+					for (numStars = 0; numStars<num; numStars++){
+						astrix = astrix +"*";
+					}
 				}
-			}
+					else if ( rowsOf==num){
+						astrix = astrix+"\n";
+						for (numStars = 0; numStars<num; numStars++){
+							astrix = astrix +"*";
+					}
+				}
+				else {
+					for (numStars = 0; numStars<num; numStars++){
+					if (numStars==0){
+					astrix = astrix+"\n*";
+					}
+					else if (numStars==num-1){
+						astrix = astrix +"*";
+					}
+					else {
+						astrix = astrix+" ";
+					}
+				}
+			} 
+		}
 		}
 		return astrix;
 	}
 
 	public static boolean hasLowercase(String word){
-		return true;
+		String tester = word.toLowerCase();
+		int letterAt = 0;
+		boolean contains = false;
+		int length = word.length();
+		boolean numTest = word.contains("1");
+		if (word.length()!=0){
+		if (numTest==false){
+		for (int lowerExist=(-1); letterAt<length; letterAt++){
+			
+			contains = (tester.charAt(letterAt)==word.charAt(letterAt));
+	
+		}
 	}
+}
+	return contains;
+	
+}
 }
